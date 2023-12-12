f3b4ff95c495d77214782bd68f6b689d8d705e86
saksham1236 <saksham1236@gmail.com>
2023-12-12 03:04:02 -0500

    Testing cheerio

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cheerio from 'cheerio';
+import pretty from 'pretty';
 
+
+const markup = `
+<ul class="fruits">
+  <li class="fruits__mango"> Mango </li>
+  <li class="fruits__apple"> Apple </li>
+</ul>
+`;
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
+app.get('/', (req:any, res:any):void => {
+    res.send(`<h1>Welcome to my Express App </h1>`)
+});
+
+const $ = cheerio.load(markup);
+
+console.log(pretty($.html()));
+
+const mango = $(".fruits__mango");
+console.log(mango.html());
+
+app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+})
