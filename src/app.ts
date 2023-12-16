e306edde8af37e6bf8c25a04ff42c0d8f8bc29d0
saksham1236 <saksham1236@gmail.com>
2023-12-16 03:16:27 -0500

    Adding fetch endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import pretty from 'pretty';
 import axios from 'axios';
  //Import Functions
 import { fetchPageTenesseWarn } from './functions/fetchTennesseWarn'
-
+import { fetchDatabase } from './functions/utlis/fetchDatabase';
 import { writeToDatabase } from './functions/utlis/writeDatabase'
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -18,26 +19,20 @@ app.get('/', (req:any, res:any):void => {
     res.send(`<h1>Welcome to my Express App </h1>`)
 });
 
-
-// async function scrapeData() {
-//   axios
-//   .get(url)
-//   .then((res) => {
-//     const { data } = res;
-//     const $ = cheerio.load(data);
-//     const list = $("#main > div > div.col-lg-8.col-md-8.col-sm-8.col-xs-12.content > article > div:nth-child(2) > div:nth-child(4)");
-//     console.log(list);
-//   })
-//   .catch((err) => {
-//     console.log(`fetching failed: ${err}`);
-//   })
-// }
-
 fetchPageTenesseWarn(url).then((data) => {
   tenesseData = data.convertedData;
   writeToDatabase(tenesseData)
 }).then;
 
+app.get(`/list`, async (req:any, res:any):Promise<void> => {
+  fetchDatabase()
+  .then((data:any) => {
+    res.send(200).json(data);
+  })
+  .catch((error) => {
+    res.send(500).json({error: `error occured: ${error}`})
+  })
+})
 
 app.get('/tenesse', (req:any, res:any):void => {
   res.json(tenesseData);
